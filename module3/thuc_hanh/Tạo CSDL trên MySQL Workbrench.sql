a7461554d09a02be0d27b1bee56940004e6107d3
Huy Duong <quochuyduongdn97@gmail.com>
2021-09-22 10:34:01 +0700

    nop bai tap

---
+++ module3/thuc_hanh/Tạo CSDL trên MySQL Workbrench.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS manager_student;
+CREATE DATABASE IF NOT EXISTS manager_student;
+
+
---
+++ module3/thuc_hanh/Xoá CSDL trên MySQL Workbrench.sql
@@ -0,0 +1,3 @@
+
+CREATE DATABASE IF NOT EXISTS manager_student;
+DROP DATABASE IF EXISTS manager_student;
