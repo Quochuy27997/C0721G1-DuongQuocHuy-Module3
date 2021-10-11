914e5671b5f203541bb0fe6edc50af879e635386
Quochuy27997 <quochuyduongdn97@gmail.com>
2021-10-11 23:15:22 +0700

    up b

---
+++ module3/_12_JDBC&CRUD/thuc_hanh_bai_tap/ung_dung_quan_li_khach_hang/src/main/java/com/codegym/model/repository/IUserDAO.java
@@ -0,0 +1,20 @@
+package com.codegym.model.repository;
+
+import com.codegym.model.bean.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IUserDAO {
+    public void insertUser(User user) throws SQLException;
+
+    public User selectUser(int id);
+
+    public List<User> selectAllUsers();
+
+    public boolean deleteUser(int id) throws SQLException;
+
+    public boolean updateUser(User user) throws SQLException;
+
+    List<User> find(String country) throws SQLException;
+}
